9234ba9bf270a6416e7ce0c70634ec9dc72c2448
MarkTiger <marco.tiger.98@gmail.com>
2021-09-22 00:31:54 +0700

    Server url

--- config/socket.js
+++ config/socket.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import io from 'socket.io-client';
-const SOCKET_URL = 'http://192.168.100.49:4000';
+const SOCKET_URL = 'http://ecommerce.marcotiger.my.id';
 
 export const socket = io(SOCKET_URL);
 export default SocketContext = React.createContext(socket);
