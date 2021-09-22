94079a85b4b4f6d717a35237cf4e5f47ec3c8622
MarkTiger <marco.tiger.98@gmail.com>
2021-09-22 15:24:47 +0700

    Fix midtrans on cancel/success redirections

--- screens/MidtransScreen.jsx
+++ screens/MidtransScreen.jsx
@@ -1,26 +1,24 @@
-import React from 'react';
+import React, { Component, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { WebView } from 'react-native-webview';
 
-export default function MidtransScreen({ route, navigation }) {
-  console.log(route.params);
+export default class MidtransScreen extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <WebView
-        onNavigationStateChange={(navState) => {
-          console.log(navState);
-          if (navState.url.includes(`${route.params}#/success`)) {
-            setTimeout(() => {
-              navigation.replace('User');
-            }, 2000);
-          }
-        }}
-        source={{ uri: route.params }}
-      />
-    </SafeAreaView>
-  );
+  componentWillUnmount() {
+    this.props.navigation.replace('User');
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <WebView source={{ uri: this.props.route.params }} />
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
